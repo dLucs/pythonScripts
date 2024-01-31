8f33239c201967473c124115dd60afb5376eb5bc
dLucs <99974795+dLucs@users.noreply.github.com>
2024-01-31 11:44:35 +0100

    Update bingSearches.py

--- bingSearches.py
+++ bingSearches.py
@@ -1,8 +1,12 @@
-import webbrowser
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 import time
-import random
+
+browser = webdriver.Edge()
+browser.get("https://www.bing.com/?setlang=es")
 
 for i in range(30):
-    url = "https://www.bing.com/search?q=" + str(random.randint(1, 1000000000))
-    webbrowser.open(url)
+    search_box = browser.find_element(By.NAME, "q")
+    search_box.send_keys(i)
+    search_box.submit()
     time.sleep(6)
